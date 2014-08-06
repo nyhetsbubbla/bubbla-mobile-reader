5bfcbbaa5105279adbf8ecb38cdcb569864c8b6f
Levon Saldamli <levon.sa@gmail.com>
2014-08-06 12:31:57 +0200

    Improved icon, added header icon

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -47,6 +47,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/bubbla/nyheter');
+  $urlRouterProvider.otherwise('/app/bubbla/Senaste');
 });
 
