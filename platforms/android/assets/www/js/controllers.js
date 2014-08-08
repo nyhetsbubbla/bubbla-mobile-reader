f5df757b399e145b84e3015526ef3f51f60de7bc
Levon Saldamli <levon.sa@gmail.com>
2014-08-08 19:47:25 +0200

    android build

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -7,7 +7,9 @@ angular.module('starter.controllers', [])
 .controller('BubbelCtrl', function($scope, $stateParams, $window, FeedService) {
   var pageId = $stateParams.pageId;
   $scope.title = FeedService.getTitle(pageId);
-  $scope.entries = FeedService.getEntries(pageId);
+  FeedService.getEntries(pageId).then(function (entries) {
+    $scope.entries = entries;
+  });
 
   var openUrl = function (prefix, relativeUrl) {
     var url = prefix + relativeUrl;
@@ -18,10 +20,9 @@ angular.module('starter.controllers', [])
   }
 
   $scope.doRefresh = function () {
-    var d = FeedService.refresh();
-    d.then(function () {
-      $scope.title = FeedService.getTitle(pageId);
-      $scope.entries = FeedService.getEntries(pageId);
+    $scope.title = FeedService.getTitle(pageId);
+    FeedService.getEntries(pageId).then(function (entries) {
+      $scope.entries = entries;
     }).finally(function () {
       $scope.$broadcast('scroll.refreshComplete');
     });
@@ -47,8 +48,8 @@ angular.module('starter.controllers', [])
   return FeedService.init();
 }])
 
-.factory('FeedService', ["$http", function ($http) {
-  var loadJson = function (url) {
+.factory('FeedService', ["$http", "$q", function ($http, $q) {
+  var loadRssJson = function (url) {
     var googleUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?' +
       'v=1.0&num=100&callback=JSON_CALLBACK&q=';
     var fullUrl = googleUrl + encodeURIComponent(url);
@@ -59,51 +60,94 @@ angular.module('starter.controllers', [])
   var feed = {};
   var categories = {};
 
-  var initCategories = function (entries) {
-    var el = entries.length;
-    for (var e=0; e<el; e++) {
-      var entry = entries[e];
-      var cl = entry.categories.length;
-      for (var c=0; c<cl; c++) {
-        var entryCat = entry.categories[c];
-        var cat = categories[entryCat];
-        if (!cat) {
-          cat = [];
-          categories[entryCat] = cat;
-        }
-        cat.push(entry);
+  var initCategories = function (categoriesJson) {
+    if (!categoriesJson || 
+      typeof categoriesJson !== "object" ||
+      Object.keys(categoriesJson) < 1) {
+      categoriesJson = { "Senaste": "http://bubb.la/rss/nyheter" };
+    }
+    console.log("categoriesJson: " + JSON.stringify(categoriesJson));
+    var keys = Object.keys(categoriesJson);
+    var cl = keys.length;
+    console.log("CL: " + cl);
+    for (var c=0; c<cl; c++) {
+      var cat = keys[c];
+      var link = categoriesJson[cat];
+      console.log("CAT: " + cat);
+      categories[cat] = {
+        "link": link
       }
     }
-    console.log("Found categories: " + JSON.stringify(Object.keys(categories)));
   };
 
   var me = {};
   me.init = function () {
-    var bubblaUrl = "http://bubb.la/rss/nyheter";
-    console.log("Loading " + bubblaUrl);
-    return loadJson(bubblaUrl)
-      .then(function (response) {
-        //console.log("loadJson response: " + JSON.stringify(response));
+    var feedsJsonUrl = "http://bubb.la/rss_feeds.json";
+    console.log("loading categories: " + feedsJsonUrl);
+    
+    var tmpDeferred = $q.defer();
+    var categoriesPromise = tmpDeferred.promise;
+    var tmpData = {
+      "Senaste": "http://bubb.la/rss/nyheter",
+      "Världen": "http://bubb.la/rss/varlden",
+      "Sverige": "http://bubb.la/rss/sverige",
+      "Blandat": "http://bubb.la/rss/blandat",
+      "Europa": "http://bubb.la/rss/europa",
+      "USA": "http://bubb.la/rss/usa",
+      "Politik": "http://bubb.la/rss/politik",
+      "Ekonomi": "http://bubb.la/rss/ekonomi",
+      "Teknik": "http://bubb.la/rss/teknik",
+      "Vetenskap": "http://bubb.la/rss/vetenskap"
+    };
+    setTimeout(function () {
+      tmpDeferred.resolve({ "data": tmpData });
+    }, 0);
+
+    var categoriesPromise = $http.get(feedsJsonUrl);
+
+    return categoriesPromise.then(function (response) {
+      var json = response.data;
+      console.log("received categories: " + JSON.stringify(json));
+      initCategories(json);
+      var first = Object.keys(categories)[0];
+      return me.initCategory(first);
+    }, function (reason) {
+      console.error(reason);
+      initCategories();
+      var first = Object.keys(categories)[0];
+      return me.initCategory(first);
+    }, function (update) {
+      console.log("update: " + update);
+      return update;
+    });
+  };
+
+  me.initCategory = function (category) {
+    console.log("Loading category " + category);
+    var categoryUrl = categories[category].link;
+    console.log("Loading " + category + " from " + categoryUrl);
+    return loadRssJson(categoryUrl).
+      then(function (response) {
         responseData = response.data.responseData;
         feed = responseData.feed;
         if (feed.entries) {
-          initCategories(feed.entries);
+          console.log("found " + feed.entries.length + " entries");
+          return feed.entries;
+        } else {
+          console.error("No feed.entries in " + JSON.stringify(feed));
+          return [];
         }
-        return me;
       }, function (reason) {
-        console.log("loadJson error: " + reason);
-        alert("Failed to load feed from " + bubblaUrl);
+        console.log("loadRssJson error: " + reason);
+        alert("Failed to load feed from " + categoryUrl);
+        return [];
       }, function (update) {
-        console.log("loadJson notification: " + update);
+        console.log("loadRssJson notification: " + update);
       });
   };
 
   me.getEntries = function (pageId) {
-    if (pageId && pageId !== "nyheter" && categories[pageId]) {
-      return categories[pageId];
-    } else {
-      return feed.entries;
-    }
+    return me.initCategory(pageId);
   };
 
   me.getCategories = function () {
@@ -112,13 +156,13 @@ angular.module('starter.controllers', [])
 
   me.getTitle = function (pageId) {
     if (pageId === "nyheter" || !categories[pageId]) {
-      return "Senaste"
+      return "Senaste";
     }
     return pageId;
   };
 
-  me.refresh = function () {
-    return me.init();
+  me.version = function () {
+    return '0.1';
   };
 
   return me;
