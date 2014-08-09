b319c17dc58459edff5c9b9aed80795d81390a82
Levon Saldamli <levon.sa@gmail.com>
2014-08-10 00:04:58 +0200

    Icons for google play

--- resources/makeAppStoreIcon.sh
+++ resources/makeAppStoreIcon.sh
@@ -1,1 +1,5 @@
 convert bubbla4-padded-transp.png -background transparent -gravity center -scale 1000x1000 -extent 1024x1024 appStoreIcon2.png
+android
+convert bubbla4-padded-transp.png -background transparent -gravity center -scale 400x400 -extent 512x512 googlePlayIcon.png
+convert bubbla4-padded-transp.png -background transparent -gravity center -scale 400x400 -extent 1024x500 googlePlayIconWide.png
+convert bubbla4-padded-transp.png -background transparent -gravity center -scale 100x100 -extent 180x120 googlePlayIconMarketing.png
