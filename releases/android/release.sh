b0a0876eb4132dbd65a41def9b2a03fdf295720a
Levon Saldamli <levon.sa@gmail.com>
2014-08-10 00:38:53 +0200

    Android release 1.0.0

---
+++ releases/android/release.sh
@@ -0,0 +1,5 @@
+cordova plugin rm org.apache.cordova.console
+cordova build --release android
+cd releases/android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore market-release-key.keystore ../../platforms/android/ant-build/bubbel-release-unsigned.apk releasekey -signedjar bubbel-release-signed.apk
+zipalign -v 4 bubbel-release-signed.apk bubbla-1.0.0.apk
